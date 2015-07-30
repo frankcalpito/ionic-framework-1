45654ba9497cb4a1f2e79a43cee5d8a3ebd5e32d
Franklin Castro Calpito Jr <franklincastrocalpito@gmail.com>
2015-07-30 13:43:36 +0800

    Db update for ms sql

--- candidatesionic/www/db/getRecords.php
+++ candidatesionic/www/db/getRecords.php
@@ -3,16 +3,32 @@ header("Access-Control-Allow-Origin: *");
 // header("Content-Type: application/json; charset=UTF-8");
 include('../partials/dbconfig.php');
 
-$query="select * from tblstudents";
-$result = $conn->query($query) or die($conn->error.__LINE__);
+// $query="select * from tblstudents";
+// $result = $conn->query($query) or die($conn->error.__LINE__);
 
-$arr = array();
+// $arr = array();
+
+// if($result->num_rows > 0) {
+// 	while($row = $result->fetch_assoc()) {
+// 		$arr[] = $row;	
+// 	}
+// }
+
+$query = "SELECT * FROM tblstudents";
+$result = sqlsrv_query($conn, $query);
+
+if( $result === false ) {
+     die( print_r( sqlsrv_errors(), true));
+}
 
 if($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
+	while($row = sqlsrv_fetch_object()) {
 		$arr[] = $row;	
 	}
 }
 
-$json_response = json_encode($arr);
-echo $json_response;
+	sqlsrv_free_stmt($result);
+    sqlsrv_close($conn);
+
+	$json_response = json_encode($arr);
+	echo $json_response;
--- candidatesionic/www/partials/dbconfig.php
+++ candidatesionic/www/partials/dbconfig.php
@@ -6,8 +6,26 @@
 // $mysql_password = "Admin123";
 
 // Development
-$mysql_host = "https://192.168.0.101";
-$mysql_database = "db_student";
-$mysql_user = "root";
-$mysql_password = "";
-$conn = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+// $mysql_host = "https://192.168.0.101";
+// $mysql_database = "db_student";
+// $mysql_user = "root";
+// $mysql_password = "";
+// $conn = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+
+//SQL Server
+	
+	$conn;
+
+    try
+    {
+        $serverName = "tcp:qhkdzwx2fn.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"dbTest",
+            "Uid"=>"farfd4323fs", "PWD"=>'@zuR3s3rvER');
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
